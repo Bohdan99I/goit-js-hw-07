2d83fbe3c9fbc23fcda02a74d387ddba3b19a7e7
Bohdan99I <bohdan.voiyov@gmail.com>
2023-05-22 19:59:53 +0300

    Task #2

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,34 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
 console.log(galleryItems);
+
+const gallery = document.querySelector(".gallery");
+
+//Створюємо розмітку
+function createGalleryItem(galleryItems) {
+      return galleryItems
+    .map(({ preview, original, description }) => {
+      return `<li class="gallery__item">
+        <a class="gallery__link" href="${original}">
+          <img
+            class="gallery__image"
+            src="${preview}"
+            data-source="${original}"
+            alt="${description}"
+          />
+        </a>
+      </li>`;
+    })
+    .join("");
+}
+
+//Додаємо елементи img до HTML
+const items = createGalleryItem(galleryItems);
+gallery.insertAdjacentHTML("beforeend", items);
+
+//Працює бібліотека
+const lightboxGallery = new SimpleLightbox(".gallery a", {
+    captionsData: "alt",
+    captionsDelay: 250,
+})
