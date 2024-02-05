16246915a9523e50e7b8e2c0941f1da3eba3cb78
Bohdan99I <bohdan.voiyov@gmail.com>
2024-02-05 20:44:53 +0200

    fix_tips_01-02.js

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,11 +1,10 @@
 import { galleryItems } from "./gallery-items.js";
-// Change code below this line
 
 const gallery = document.querySelector(".gallery");
 
 //Створюємо розмітку
 function createGalleryItem(galleryItems) {
-      return galleryItems
+  return galleryItems
     .map(({ preview, original, description }) => {
       return `<li class="gallery__item">
         <a class="gallery__link" href="${original}">
@@ -29,30 +28,33 @@ gallery.insertAdjacentHTML("beforeend", items);
 gallery.addEventListener("click", onGalleryClick);
 
 function onGalleryClick(event) {
-    // Прибираємо нативну поведінку
-    event.preventDefault();
-    //Спрацювання функції лише по кліку на зображення
-    const isImgClick = event.target.classList.contains("gallery__image");
-    if (!isImgClick) {
-        return;
+  // Прибираємо нативну поведінку
+  event.preventDefault();
+  //Спрацювання функції лише по кліку на зображення
+  const isImgClick = event.target.classList.contains("gallery__image");
+  if (!isImgClick) {
+    return;
+  }
+  // Бібліотека для відкриття модалки
+  const instance = basicLightbox.create(
+    `<img src="${event.target.dataset.source}" width="800" height="600">`,
+    {
+      onShow: () => {
+        document.addEventListener("keydown", keyEsc);
+      },
+      onClose: () => {
+        document.removeEventListener("keydown", keyEsc);
+      },
     }
-    // Бібліотека для відкриття модалки
-    const instance = basicLightbox.create(`<img src="${event.target.dataset.source}" width="800" height="600">`, {
-        onShow: () => {
-            document.addEventListener("keydown", keyEsc);
-        },
-        onClose: () => {
-            document.removeEventListener("keydown", keyEsc);
-        },
-    });
-    //Закриття модалки через "Escape"
-    const keyEsc = (event) => {
-        if (event.key === "Escape") {
-            instance.close()
-        }
-    };
+  );
+  //Закриття модалки через "Escape"
+  const keyEsc = (event) => {
+    if (event.key === "Escape") {
+      instance.close();
+    }
+  };
 
-    instance.show();
+  instance.show();
 }
 
 console.log(galleryItems);
